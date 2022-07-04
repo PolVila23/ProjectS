9df1ca84dd47e28ed4a31e94b6bb32f27d94ad19
perecarrillo <pere.carrillo.barrera@estudiantat.upc.edu>
2022-07-04 14:12:40 +0200

    Start/Stop + Finish
    S'ha d'arreglar el codi i separar-lo en diferents scripts.

--- ProjectS/Assets/Scripts/Spaceship.cs
+++ ProjectS/Assets/Scripts/Spaceship.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Spaceship : MonoBehaviour
 {
@@ -13,44 +14,52 @@ public class Spaceship : MonoBehaviour
 
     public static Attractor[] Attractors;
 
+    private Finish finish;
+
     private Vector2 posAnt;
+
+    private bool play;
+
+    private GameObject StartButton;
+    private GameObject StopButton;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        play = false;
         rb = GetComponent<Rigidbody2D>();
         Attractors = FindObjectsOfType<Attractor>();
-
-        Vector2 force = new Vector2(0, initialSpeed);
-        
-        rb.AddForce(force);
-
-        posAnt = transform.position;
+        finish = FindObjectOfType<Finish>();
+        StartButton = GameObject.Find("StartButton");
+        StopButton = GameObject.Find("StopButton");
+        StopButton.SetActive(false);
     }
 
     void FixedUpdate()
     {
-        Vector2 totalForce = new Vector2(0, 0);
-        foreach (Attractor attractor in Attractors)
-        {
-            Vector2 force = new Vector2(0, 0);
-            Attract(attractor, ref force);
-            totalForce += force;
+        if (play) {
+            Vector2 totalForce = new Vector2(0, 0);
+            foreach (Attractor attractor in Attractors)
+            {
+                Vector2 force = new Vector2(0, 0);
+                Attract(attractor, ref force);
+                totalForce += force;
+            }
+
+            rb.AddForce(totalForce);
+
+            Vector2 posNou = transform.position;
+            Vector2 direction = posNou - posAnt;
+
+            if (direction.magnitude != 0)
+            {
+                float lookAngle = Mathf.Atan2(direction.y, direction.x)  * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(lookAngle - 90, Vector3.forward);
+            }
+
+            posAnt = transform.position;
         }
-
-        rb.AddForce(totalForce);
-
-        Vector2 posNou = transform.position;
-        Vector2 direction = posNou - posAnt;
-
-        if (direction.magnitude != 0)
-        {
-            float lookAngle = Mathf.Atan2(direction.y, direction.x)  * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(lookAngle - 90, Vector3.forward);
-        }
-
-        posAnt = transform.position;
     }
 
     void Attract(Attractor a, ref Vector2 force)
@@ -70,6 +79,31 @@ public class Spaceship : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Haha t'has xocat lol");
+        if (other.name == finish.name) {
+            SceneManager.LoadScene("YouWin");
+        }
+        else {
+            Debug.Log("Haha t'has xocat lol");
+            StopLevel();
+        }
+    }
+
+    public void StartLevel() {
+        play = true;
+
+        Vector2 force = new Vector2(0, initialSpeed);
+        
+        rb.AddForce(force);
+
+        posAnt = transform.position;
+    }
+
+    public void StopLevel() {
+        play = false;
+        rb.velocity = new Vector2(0, 0);
+        transform.position = new Vector2(-0.15f, -7.12f);
+        transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
+        StartButton.SetActive(true);
+        StopButton.SetActive(false);
     }
 }
